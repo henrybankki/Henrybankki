a926fae208d612537e92751585d5264c480c5e49
henrybankki <atsiu143@gmail.com>
2025-07-14 17:50:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,14 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Firebase-konfiguraatio (V8)
+var firebaseConfig = {
   apiKey: "AIzaSyB3eAdsCYIFWI06DS6lj5GUMYBwooRNd_8",
   authDomain: "henrybank-12a99.firebaseapp.com",
   projectId: "henrybank-12a99",
-  storageBucket: "henrybank-12a99.firebasestorage.app",
+  storageBucket: "henrybank-12a99.appspot.com",
   messagingSenderId: "404077920890",
   appId: "1:404077920890:web:9eee6fa70b11272bb671c3",
   measurementId: "G-NC2R6N5K7M"
 };
+
+// Alusta Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
